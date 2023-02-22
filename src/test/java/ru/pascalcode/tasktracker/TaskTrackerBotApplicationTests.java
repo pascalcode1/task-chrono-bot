2beaf35baf38678bd8d550b725cec2f8ba3fe508
Vadim Timofeev <pascalcode1@gmail.com>
2023-02-22 20:13:33 +0500

    Second commit

---
+++ src/test/java/ru/pascalcode/tasktracker/TaskTrackerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pascalcode.tasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
